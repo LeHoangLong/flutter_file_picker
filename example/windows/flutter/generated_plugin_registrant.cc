7b3ee036e17a19f5302e416d5511b526b07d97a0
Le Hoang Long <le.hoang.long@outlook.com>
2020-12-05 17:48:47 +0800

    Updated gitignore

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <file_picker/file_picker_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FilePickerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FilePickerPlugin"));
+}
